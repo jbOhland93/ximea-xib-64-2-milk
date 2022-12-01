f6eb096ff92a76dadb3d8b43abdd99af02a650b5
Jonas B. Ohland <jonas.ohland@polytechnique.edu>
2022-12-01 15:30:16 +0100

    Made first rudimentary CLI.

--- src/sources/UserInputHandler.cpp
+++ src/sources/UserInputHandler.cpp
@@ -6,38 +6,30 @@
 using namespace std;
 
 void UserInputHandler::core()
-{
-    bool closing = false;
+{   
+    const char * prompt = "Enter command: ";
+    const char * answer = "You entered: ";
 
-    // Print a welcome message.
-    printf("\n\n === Welcome to the deformable-mirror-server application, wrapping the control libraries of ISP System. ===\n");
+    initscr();
+    addstr("\n\n === Welcome to our WIP CLI. ===\n");
+    addstr("\nEnter any string and hit enter to display the response.\n");
+    addstr("Enter \"quit\" to exit the CLI.\n");
+    mvprintw(LINES-2,0,prompt);
 
-    // Parse keyboard input (nonblocking).
-    //initscr();
-    //cbreak();
-    //noecho();
-    //scrollok(stdscr, TRUE);
-    //nodelay(stdscr, TRUE);
+    while (true) {
+        wmove(stdscr, LINES-2, strlen(prompt));
+        clrtoeol();
 
-char mesg[]="Enter a string: ";         /* message to be appeared on the screen */
- char str[80];
- int row,col;                           /* to store the number of rows and *
-                                         * the number of colums of the screen */
- initscr();                             /* start the curses mode */
- getmaxyx(stdscr,row,col);              /* get the number of rows and columns */
- mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-                                /* print the message at the center of the screen */
- getstr(str);
- mvprintw(LINES - 2, 0, "You Entered: %s", str);
- getch();
- endwin();
-
-    /*while (true) {
-        if (getch() == 'q') {
-            printw("You pressed q\n");
+        char str[1024];
+        getstr(str);
+        if (0 == strcmp(str, "quit"))
             break;
+        else
+        {
+            mvprintw(LINES-1, 0, answer);
+            clrtoeol();
+            addstr(str);
         }
-        //printw("Running\n");
     }
-    endwin();*/
+    endwin();
 }
